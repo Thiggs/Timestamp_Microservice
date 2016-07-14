79ac17bf87c6a4737c7e872757f846efde2e12dd
Tyler <realpsych22@gmail.com>
2016-07-14 20:01:16 +0000

    update

--- timestamp/server.js
+++ timestamp/server.js
@@ -4,16 +4,23 @@ var app = express();
 var date;
 var unix;
 
-app.get("/", function(req, res){
-    if(date = new Date(req)){
+app.get("/", function (req, res){
+    if(isNaN(req)){
         date = new Date(req);   
         unix = date.getTime() / 1000;
     }
-    else if(unix = req.getTime()/1000){
-        req.getTime()/1000;
+    else {
+        unix = req;
         date = new Date(unix);
     }
-    console.log(unix, date);
+    if(date && unix){
+    res.write("unix: " + unix + ", natural: " + date);
+    res.send();
+    }
+    else{
+        console.log("unix: " + null + ", natural: " + null);
+        res.send();
+    }
 });
 
 app.listen(8080);
